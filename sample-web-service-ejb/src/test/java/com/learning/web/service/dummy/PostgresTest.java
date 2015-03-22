39b576ed6db7427a202c49a04ec2196a8bfc90f3
danicu <d.cunniffe.ie@gmail.com>
2015-03-22 18:46:35 +0000

    Updating gitignore, wildfly & arquillian version, adding test with postgresql db added

--- sample-web-service-ejb/src/test/java/com/learning/web/service/dummy/PostgresTest.java
+++ sample-web-service-ejb/src/test/java/com/learning/web/service/dummy/PostgresTest.java
@@ -2,6 +2,8 @@ package com.learning.web.service.dummy;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.File;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -12,13 +14,14 @@ import org.jboss.arquillian.transaction.api.annotation.Transactional;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @Transactional(TransactionMode.ROLLBACK)
 @RunWith(Arquillian.class)
-public class DummyClassTest {
+public class PostgresTest {
 
 	@PersistenceContext(name = "test")
 	private EntityManager entityManager;
@@ -34,9 +37,11 @@ public class DummyClassTest {
 
 	@Deployment
 	public static WebArchive createDeployment() {
+		File[] libraries = Maven.resolver().loadPomFromFile("pom.xml").resolve("postgresql:postgresql")
+				.withTransitivity().asFile();
 
 		return ShrinkWrap.create(WebArchive.class, "test.war").addAsWebInfResource("web.xml").addClass(DummyUser.class)
-				.addAsResource("test-persistence.xml", "META-INF/persistence.xml")
+				.addAsResource("test-persistence.xml", "META-INF/persistence.xml").addAsLibraries(libraries)
 				.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
 	}
 
